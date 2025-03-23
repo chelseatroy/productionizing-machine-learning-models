652bd1d157e98825b2278f5196890588e460cbbc
Chelsea Troy <chelsea.dommert@gmail.com>
2025-03-22 19:16:14 -0500

    Add a start script so people don't have to remember, or copy and paste, the command for running this

---
+++ start_docker.sh
@@ -0,0 +1,5 @@
+docker run --rm -e AWS_ACCESS_KEY_ID=your-access-key \
+    -e AWS_SECRET_ACCESS_KEY=your-secret-access-key \
+    -e AWS_DEFAULT_REGION=your-bucket-region \
+    -e METAFLOW_USER=your-name \
+    sentiment-flow
